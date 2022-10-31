e7d9f0935105f18f457152e657ddda64b47f953e
1sxyzn <soyun9902@ewhain.net>
2022-10-31 16:46:23 +0900

    Init: Initial setting

---
+++ src/main/java/com/website/stw/StwApplication.java
@@ -0,0 +1,13 @@
+package com.website.stw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/website/stw/StwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.stw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
